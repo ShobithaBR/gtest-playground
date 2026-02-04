f761fbc391c194de3d19a79c4e3bc27f3a6094c5
ShobithaBR <BanavaraRamesh.Shobitha@in.bosch.com>
2026-02-04 12:09:09 +0530

    testdemo4

--- testdemo.test.cpp
+++ testdemo.test.cpp
@@ -6,7 +6,7 @@ TEST(testdemo,addfunctio){
     //arrange - preconditions, prepare input data
     int operand1 = 10;
     int operand2 = 20;
-    int actualresult = 0
+    int actualresult = 0;
     int expectedresult = 30;
     //act - invoke code under test
     actualresult = add(operand1,operand2);
